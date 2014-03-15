e50a027ad08ffa32e973224b5f089acaddddf2c0
pranavpr <pranav.iitkgp@gmail.com>
2014-03-15 17:27:08 +0530

    Post Data Processing

--- index.php
+++ index.php
@@ -66,25 +66,12 @@ if ($user) {
 
 	
 			 <div class="post">
-<?php
-# This function reads your DATABASE_URL configuration automatically set by Heroku
-# the return value is a string that will work with pg_connect
-function pg_connection_string() {
-  return "dbname=d56jl9rpuri4us host=ec2-184-73-194-196.compute-1.amazonaws.com port=5432 user=bsegsbwhqrwkvt password=SluOhNXZ9yUxlQaHOtt_8muJCN sslmode=require";
-}
- 
-# Establish db connection
-$db = pg_connect(pg_connection_string());
-if (!$db) {
-    echo "Database connection error.";
-    exit;
-}
-$result = pg_query($db, "select * from Persons");
-?>
 <form name="input" action="submit.php" method="post">
-<textarea rows="4" cols="50">
+<textarea name="comment" rows="4" cols="50">
 Tell us about your Figo experience.
 </textarea><br/>
+<input type="hidden" name="fbid" value="<?php $user_profile->id ?>">
+<input type="hidden" name="name" value="<?php $user_profile->name ?>">
 <input type="submit" value="Submit">
 </form>
             </div><!--End Blog Post-->
---
+++ submit.php
@@ -0,0 +1,23 @@
+<?php
+$fbid=$_POST['fbid']; 
+$name=$_POST['name']; 
+$comment=$_POST['comment']; 
+# This function reads your DATABASE_URL configuration automatically set by Heroku
+# the return value is a string that will work with pg_connect
+function pg_connection_string() {
+  return "dbname=d56jl9rpuri4us host=ec2-184-73-194-196.compute-1.amazonaws.com port=5432 user=bsegsbwhqrwkvt password=SluOhNXZ9yUxlQaHOtt_8muJCN sslmode=require";
+}
+ 
+# Establish db connection
+$db = pg_connect(pg_connection_string());
+if (!$db) {
+    echo "Database connection error.";
+    exit;
+}
+$result = pg_query($db, "insert into Persons (Name, FacebookId, Comment) values ('$name', '$fbid', '$comment')");
+//dump the result object
+var_dump($result);
+
+// Closing connection
+pg_close($db);
+?>

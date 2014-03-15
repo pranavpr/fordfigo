bafd6e9607149878dcd1d7ec7972523d4b1defc9
pranavpr <pranav.iitkgp@gmail.com>
2014-03-15 17:33:31 +0530

    Post Data Processing

--- index.php
+++ index.php
@@ -70,8 +70,8 @@ if ($user) {
 <textarea name="comment" rows="4" cols="50">
 Tell us about your Figo experience.
 </textarea><br/>
-<input type="hidden" name="fbid" value="<?php $user_profile->id ?>">
-<input type="hidden" name="name" value="<?php $user_profile->name ?>">
+<input type="text" name="fbid" value="<?php $user_profile->id ?>"><br/>
+<input type="text" name="name" value="<?php $user_profile->name ?>"><br/>
 <input type="submit" value="Submit">
 </form>
             </div><!--End Blog Post-->
